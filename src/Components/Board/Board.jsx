6e77703e91a67c4bd5c41e46f7b046dbc02c0676
Brittany Price <bprice0496@gmail.com>
2025-08-07 23:05:21 -0400

    Added numbers to the bottom of the game

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,8 +1,11 @@
 import BoardStyles from '../../css/Board.module.css';
 import Squares from './Square';
+import { gameState } from '../../Store/GameState';
 
 function Board() {
+    const { time, isPause, startGAme, mistake, totalMistakes, isComplete, mode, changeQBoard, tryAgain } = gameState();
     const squares = Array(3).fill(Array(3).fill(null));
+    const numbers = Array(9).fill(null);
     return (
         <>
           <div className={BoardStyles.Main}>
@@ -13,6 +16,14 @@ function Board() {
                     ))}
                 </div>
             ))}
+          </div>
+          {/* Row of numbers below the board */}
+          <div className={BoardStyles.NumContainer}>
+              {numbers.map((_,i) => (
+                    <span onClick={() => changeQBoard(i + 1)} key={i} className={BoardStyles.NumRow}>
+                        {i + 1}
+                    </span>
+              ))}
           </div>
         </>
     )
